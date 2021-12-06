6005055aa22d7a9fd1ea7d83b247bb40c065fd2a
Paccurti <paccurtijr@gmail.com>
2021-12-05 21:40:12 -0300

    final api

--- app/models/cliente.js
+++ app/models/cliente.js
@@ -1,11 +1,18 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+/**
+ * Arquivo: produto.js
+ * Author: Pedro Curti
+ * Descrição: arquivo responsável por tratar o modelo da classe 'Cliente'
+ * Data: 04/12/2021
+ */
 
-var ClienteSchema = new Schema({
-    nome: String,
-    cpf: Number,
-    dataNascimento: Date,
-    telefone: Number
-});
-
-module.exports = mongoose.model('Cliente', ClienteSchema);
+ var mongoose = require('mongoose');
+ var Schema = mongoose.Schema;
+ 
+ var ClienteSchema = new Schema({
+     nome: String,
+     cpf: Number,
+     dataNascimento: Number,
+     telefone: Number
+ });
+ 
+ module.exports = mongoose.model('Cliente', ClienteSchema);
--- server.js
+++ server.js
@@ -1,26 +1,145 @@
+/**
+ * 
+ * Arquivo: server.js
+ * Descrição: 
+ * Author:
+ * Data de Criação: 04/12/2021
+ * 
+ */
+
+// Configurar o Setup da App:
+
+//Chamadas dos pacotes:
 var express = require('express');
 var app = express();
-var BodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var cliente = require('./app/models/cliente');
-const bodyParser = require('body-parser');
+var Cliente = require('./app/models/cliente');
+mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/node-crud-api');
+//MLocal: MongoDb:
+mongoose.connect('mongodb://localhost:27017/node-crud-api', {
+    useMongoClient: true
+});
 
+//mongoose.connect('mongodb+srv://paccurti:Pa230597@cluster0.cmtrd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+  //  useMongoClient: true
+//});
+
+//Configuração da variável app para usar o 'bodyParser()':
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+//Definindo a porta onde será executada a api:
 var port = process.env.port || 8000;
 
+//Rotas da API:
+//=============================================================================
 
-
+//Criando uma instância das Rotas via Express:
 var router = express.Router();
 
-router.get('/', function(req, res){
-    res.json({message: 'Faça seu Cadastro'})
+//Middleware para usar em todos os requests enviados para a API- Mensagem Padrão:
+router.use(function(req, res, next) {
+    console.log('Algo está acontecendo aqui....');
+    next(); //aqui é para sinalizar o prosseguimento para a próxima rota.
 });
 
+//Rota de Teste do funcionamento (acessar através: GET: http://localhost:8000/api): 
+router.get('/', function(req, res) {
+    res.json({ message: 'Cadastrar Cliente' })
+});
+
+//API's:
+//==============================================================================
+
+//Rotas que terminarem com '/clientes' (servir: GET ALL & POST)
+router.route('/clientes')
+
+    /* 1) Método: Criar Cliente (acessar em: POST http://localhost:8000/api/clientes)  */
+    .post(function(req, res) {
+        var cliente = new Cliente();
+
+        //Setando os campos do cliente (via request):
+        cliente.nome = req.body.nome;
+        cliente.cpf = req.body.cpf;
+        cliente.dataNascimento = req.body.dataNascimento;
+        cliente.telefone = req.body.telefone;
+
+        cliente.save(function(error) {
+            if(error)
+                res.send('Erro ao tentar salvar o Cliente....: ' + error);
+            
+            res.json({ message: 'Cliente Cadastrado com Sucesso!' });
+        });
+    })
+
+    /* 2) Método: Selecionar Todos Clientes (acessar em: GET http://localhost:8000/api/clientes)  */
+    .get(function(req, res) {
+        Cliente.find(function(error, clientes) {
+            if(error) 
+                res.send('Erro ao tentar Selecionar Todos os clientes...: ' + error);
+
+            res.json(clientes);
+        });
+    });
+
+    //Rotas que irão terminar em '/clientes/:cliente_id' (para: GET, PUT & DELETE: id):
+    router.route('/clientes/:cliente_id')
+
+    /* 3) Método: Selecionar por Id: (acessar em: GET http://localhost:8000/api/clientes/:cliente_id) */
+    .get(function (req, res) {
+        
+        //Função para poder Selecionar um determinado cliente por ID - irá verificar se caso não encontrar um detemrinado
+        //cliente pelo id... retorna uma mensagem de error:
+        Cliente.findById(req.params.cliente_id, function(error, cliente) {
+            if(error)
+                res.send('Id do Cliente não encontrado....: ' + error);
+
+            res.json(cliente);
+        });
+    })
+
+    /* 4) Método: Atualizar por Id: (acessar em: PUT http://localhost:8000/api/clientes/:cliente_id) */
+    .put(function(req, res) {
+
+        Cliente.findById(req.params.cliente_id, function(error, cliente) {
+            if (error) 
+                res.send("Id do Cliente não encontrado....: " + error);
+
+                 
+                cliente.nome = req.body.nome;
+                cliente.cpf = req.body.cpf;
+                cliente.dataNascimento = req.body.dataNascimento;
+                cliente.telefone = req.body.telefone;
+
+                
+                cliente.save(function(error) {
+                    if(error)
+                        res.send('Erro ao atualizar o cliente....: ' + error);
+
+                    res.json({ message: 'Cliente atualizado com sucesso!' });
+                });
+            });
+        })
+
+        /* 5) Método: Excluir por Id (acessar: http://localhost:8000/api/clientes/:cliente_id) */
+        .delete(function(req, res) {
+            
+            Cliente.remove({
+                _id: req.params.cliente_id
+                }, function(error) {
+                    if (error) 
+                        res.send("Id do Cliente não encontrado....: " + error);
+
+                    res.json({ message: 'Cliente Excluído com Sucesso!' });
+                });
+            });
+
+
+//Definindo um padrão das rotas prefixadas: '/api':
 app.use('/api', router);
 
+//Iniciando a Aplicação (servidor):
 app.listen(port);
-console.log("Iniciando a app na porta" + port);
+console.log("Iniciando a app na porta " + port);
